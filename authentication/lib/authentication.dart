cc3a60f26205152489bca64f2ed8a7c25bcdfbe4
Jasitharan <jasitharan1998@gmail.com>
2022-05-15 13:11:24 +0530

    commit

--- authentication/lib/authentication.dart
+++ authentication/lib/authentication.dart
@@ -2,5 +2,6 @@ library authentication;
 
 export 'providers/models/user_model.dart';
 export 'providers/auth_provider.dart';
+export 'providers/apple_signin_available.dart';
 export 'auth_settings.dart';
 export './repository/api/auth_api.dart';
